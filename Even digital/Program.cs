16d5b351b067a26c8668815c1d7697b85a9aeb1b
Vlad Belyaev <mr.vladiss@mail.ru>
2023-09-19 10:17:19 +0300

    Even digital

---
+++ Even digital/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num%2 > 0)
+Console.WriteLine("Число Не является четным");
+else
+Console.WriteLine("Число является четным");
