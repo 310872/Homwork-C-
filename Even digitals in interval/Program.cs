44f437fed17f2b593841410ef8d0437212999b03
Vlad Belyaev <mr.vladiss@mail.ru>
2023-09-19 11:19:44 +0300

    Even digitals in interval

---
+++ Even digitals in interval/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count <= n)
+{
+    if(count%2 > 0)
+    {
+        Console.WriteLine(" ");
+    }
+    else
+    {
+        Console.WriteLine($"четные: {count}");
+            }
+count = count + 1;
+}
