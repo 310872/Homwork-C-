ee49d90d3d5ff924bfec2eda53da26f91af48da3
Vlad Belyaev <mr.vladiss@mail.ru>
2023-09-19 09:00:20 +0300

    Max digital for two

---
+++ Max digital for two/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+Console.WriteLine($"max = {num1}");
+else
+Console.WriteLine($"max = {num2}");
